58af92cf443281e6dc9c04c8ccca506aed958a0c
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-04-02 12:41:10 -0300

    Add DAP config for C, C++ and Rust.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,9 +8,40 @@ return {
 		},
 		config = function()
 			require("dapui").setup()
+
+			local dap = require("dap")
+
+			dap.adapters.lldb = {
+				type = "executable",
+				command = "/opt/homebrew/Cellar/llvm/20.1.1/bin/lldb-dap",
+				name = "lldb",
+			}
+
+			dap.configurations.cpp = {
+				{
+					name = "Debug",
+					type = "lldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+					end,
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+					args = {},
+				},
+				{
+					name = "Dummy",
+				}
+			}
+
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
 		end
 	},
 	{
 		'leoluz/nvim-dap-go',
-	}
+		config = function()
+			require("dap-go").setup()
+		end
+	},
 }
