02a055b170ad7058ff2ca57dd3b84833bd99799b
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-04-30 20:09:12 -0300

    Adjust lsp inlay hint configuration.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,28 +2,27 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		local cap = require "cmp_nvim_lsp".default_capabilities()
+		local on_attach = function(client, bufnr)
+			if client.server_capabilities.inlayHintProvider then
+				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			end
+		end
+
 
 		require "lspconfig".lua_ls.setup {
 			capabilities = cap,
 			settings = {
-				lua_ls = {
-					hints = {
-						assignVarialbeTypes = true,
-						compsiteLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
+				Lua = {
+					hint = {
+						enable = true,
+						setType = true,
+						paramType = true,
 					}
 				}
 			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
+			on_attach = on_attach,
 		}
+
 
 		require "lspconfig".gopls.setup {
 			capabilities = cap,
@@ -40,81 +39,53 @@ return {
 					}
 				}
 			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
+			on_attach = on_attach,
 		}
 
 		require "lspconfig".rust_analyzer.setup {
 			capabilities = cap,
 			settings = {
-				rust_analyzer = {
-					hints = {
-						assignVarialbeTypes = true,
-						compsiteLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					}
-				}
+				["rust-analyzer"] = {
+					inlayHints = {
+						lifetimeElisionHints = {
+							enable = true,
+							useParameterNames = true,
+						},
+						bindingModeHints = {
+							enable = true,
+						},
+						typeHints = {
+							enable = true,
+						},
+						chainingHints = {
+							enable = true,
+						},
+						closingBraceHints = {
+							enable = true,
+						},
+						parameterHints = {
+							enable = true,
+						},
+					},
+				},
 			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
+			on_attach = on_attach,
 		}
 
 		require "lspconfig".clangd.setup {
 			capabilities = cap,
 			cmd = {
 				"clangd",
+				"--completion-style=detailed",
+				"--inlay-hints",
 				"--enable-config",
 			},
-			settings = {
-				clangd = {
-					hints = {
-						assignVarialbeTypes = true,
-						compsiteLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					}
-				}
-			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
+			on_attach = on_attach,
 		}
 
 		require "lspconfig".pylsp.setup {
 			capabilities = cap,
-			settings = {
-				pylsp = {
-					hints = {
-						assignVarialbeTypes = true,
-						compsiteLiteralFields = true,
-						compositeLiteralTypes = true,
-						constantValues = true,
-						functionTypeParameters = true,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					}
-				}
-			},
-			on_attach = function(client, bufnr)
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-				end
-			end
 		}
+
 	end
 }
