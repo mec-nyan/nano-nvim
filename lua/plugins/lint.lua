bc58967607d765c99f38ffef16375f86cbf7f9a2
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-05-01 19:09:08 -0300

    Add linters for Lua, Go, C, C++ and Python.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,6 +6,11 @@ return {
 
 		lint.linters_by_ft = {
 			rust = { "clippy" },
+			go = { "golangcilint" },
+			c = { "cpplint" },
+			cpp = { "cpplint" },
+			lua = { "luacheck" },
+			python = { "flake8", "mypy" },
 		}
 
 		vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
