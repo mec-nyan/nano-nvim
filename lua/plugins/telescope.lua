664f3525cfdf19ab2768aec61dccf8d3d8e2cf51
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-03 20:25:42 -0300

    Fix telescope borders in Kitty.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,16 @@ return {
 	tag = '0.1.8',
 	-- or                              , branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function ()
-		local tl = require'telescope'
+	config = function()
+		local tl = require 'telescope'
 		tl.setup {
+			defaults = {
+				borderchars = {
+					prompt = { "━", "┃", "━", "┃", "╭", "╮", "╯", "╰" },
+					results = { "━", "┃", "━", "┃", "╭", "╮", "╯", "╰" },
+					preview = { "━", "┃", "━", "┃", "╭", "╮", "╯", "╰" },
+				}
+			},
 			pickers = {
 				find_files = {
 					theme = 'ivy',
