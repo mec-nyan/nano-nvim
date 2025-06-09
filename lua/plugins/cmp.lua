7d6885282c6a83b0c780e2aea7be578db2eb517f
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-09 18:32:43 -0300

    cmp: Tab for snips/completion/etc

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,6 +28,16 @@ return {
 		local cmp = require 'cmp'
 		local my_kind = require "config.kind".MyKind
 
+		local feedkey = function(key, mode)
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+		end
+
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		cmp.setup({
 			view = {
 				entries = "custom",
@@ -73,7 +83,27 @@ return {
 					else
 						fallback()
 					end
-				end)
+				end),
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif vim.fn["vsnip#available"](1) == 1 then
+						feedkey("<Plug>(vsnip-expand-or-jump)", "")
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				['S-<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif vim.fn["vsnip#available"](-1) == 1 then
+						feedkey("<Plug>(vsnip-jump-prev)", "")
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
